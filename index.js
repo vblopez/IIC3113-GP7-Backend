926fec423625ab62d916a1a3bbdf143af26ed0a6
Nicolas Karlezi <nicolaskarlezi@MacBook-Pro-de-Nicolas.local>
2022-10-03 17:31:05 -0300

    Basede datos

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const request = require("request-promise");
 const { brotliDecompressSync } = require("zlib");
 const fs = require("fs-extra");
 
+const Medicamento = require("./models/medicamento")
+
+// Conexion a base de datos
+const mongoose = require("mongoose");
+
+const user = 'nkarlezib';
+const password = 'GPTI1234';
+const url = `mongodb+srv://nkarlezib:${password}@cluster0.lbq3xfr.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(url,
+     {useNewUrlParser: true, useUnifiedTopology: true })
+     .then(()=> console.log('Base de datos conectada'))
+     .catch(e => console.log(e))
+
+
 const writestream = fs.createWriteStream("productos.csv")
 //require('events').EventEmitter.prototype._maxListeners = 0;
 
@@ -19,9 +34,9 @@ async function init(){
     });
     writestream.write('Producto|Descripcion|Precio\n');
     const titulo = $('div.product-item-info').each((i,el)=> {
-        const nombre = $(el).find('p.product-brand-name.truncate').text();
-        const descripcion = $(el).find('a.product-item-link').text();
-        const price = $(el).find('span.price').text();
+        const nombre = $(el).find('p.product-brand-name.truncate').text().trim();
+        const descripcion = $(el).find('a.product-item-link').text().trim();
+        const price = $(el).find('span.price').text().trim();
         writestream.write(`${nombre}|${descripcion}|${price}\n`)
     });
 
@@ -36,12 +51,28 @@ async function main(){
         jar: true
     
     });
-    writestream.write('Producto|Descripcion|Precio\n');
+    writestream.write('Producto, Precio\n');
     const titulo = $('div.product-item-info.type1').each((i,el)=> {
-        const nombre = $(el).find('a.product-item-link').text();
-        const descripcion = $(el).find('a.product-item-link').text();
-        const price = $(el).find('span.price').text();
-        //writestream.write(`${nombre}|${descripcion}|${price}\n`)
+        const nombre = $(el).find('a.product-item-link').text().trim();
+        //const descripcion = $(el).find('a.product-item-link').text().trim();
+        const price = $(el).find('span.price').text().trim();
+
+        try {
+            const medicamentoDB = new Medicamento({
+                "nombre": nombre,
+                "precio": price
+                });
+            
+            medicamentoDB.save();
+    
+            
+        } catch (error) {
+            console.log('error', error)
+        }
+        
+        //db.sales.insert({'nombre': `${nombre}`, 'Precio':`${price}`});
+
+        //writestream.write(`${nombre},${price}\n`)
     });
 
     
---
+++ models/medicamento.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { model } = require("mongoose");
+const Schema  = mongoose.Schema;
+
+const medicamentoSchema = new Schema({
+  
+  nombre: String,
+  precio: String,
+  
+  }
+);
+
+//creamos el modelo
+
+const Medicamento = mongoose.model("Medicamento", medicamentoSchema);
+
+module.exports = Medicamento;
